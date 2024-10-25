0c7c2297060e4339df47c53e8111d78868537641
narawaa <nashwa.ghania@ui.ac.id>
2024-10-25 16:08:35 +0700

    fix: models and data api

--- products/views.py
+++ products/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.core import serializers
+
+from products.models import Product
 
 # Create your views here.
+def get_products(request):
+    data = Product.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type = "application/json")
