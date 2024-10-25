bc41b05af2118232b0e190121b35099bd82a1221
TheoKevH <theodorekevinh@gmail.com>
2024-10-26 01:17:33 +0700

    decorate products page

--- products/views.py
+++ products/views.py
@@ -7,4 +7,11 @@ from products.models import Product
 # Create your views here.
 def get_products(request):
     data = Product.objects.all()
+
     return HttpResponse(serializers.serialize("json", data), content_type = "application/json")
+
+def show_product(request):
+    # Get all the products from the data base
+    products = Product.objects.all()
+
+    return render(request, "main.html", {"products": products})
