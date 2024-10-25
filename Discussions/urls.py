2a8a3d358dbb4855c53aa72d46937c80d3cd0529
Khairul Bintang <khairulbintang08@gmail.com>
2024-10-26 00:06:30 +0700

    Ga error lagi

--- Discussions/urls.py
+++ Discussions/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'Discussions'  # Menambahkan app_name untuk namespace
+
 urlpatterns = [
     path('create_thread/', views.create_thread, name='create_thread'),
     path('add_comment/<int:thread_id>/', views.add_comment, name='add_comment'),
--- SoundNest/urls.py
+++ SoundNest/urls.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
 
-app_name = 'Discussions'
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('Discussions.urls')),
