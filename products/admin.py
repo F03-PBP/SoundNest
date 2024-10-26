90603beebcf972157d3b44121e2bacb61a1b96ae
narawaa <nashwa.ghania@ui.ac.id>
2024-10-26 17:39:29 +0700

    fix: navbar

--- products/admin.py
+++ products/admin.py
@@ -5,7 +5,3 @@ from products.models import Product
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('product_name', 'price', 'rating', 'reviews')
-
-@admin.register(Product)
-class ProductAdmin(admin.ModelAdmin):
-    list_display = ('product_name', 'price', 'rating', 'reviews')
