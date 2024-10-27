e96ae1748908c16bef43f60d8476e891d7cbebdd
TheoKevH <theodorekevinh@gmail.com>
2024-10-27 15:37:52 +0700

    navbar (blm bisa logout)

--- products/views.py
+++ products/views.py
@@ -16,7 +16,7 @@ def get_products(request):
 @login_required()
 def show_product(request):
     # Get all the products from the data base
-    products = Product.objects.all()
+    products = Product.objects.all().order_by('-created_at')
 
     paginator= Paginator(products, 20)
 
@@ -42,11 +42,13 @@ def product_details(request, product_id):
 @csrf_exempt
 @require_POST
 def add_product(request):
-    if request.method == 'POST':
+    try:
         product_name = request.POST.get('product_name')
         price = request.POST.get('price')
         rating = request.POST.get('rating')
         reviews = request.POST.get('reviews')
+
+        rating = float(rating)
 
         # Create a new Product instance
         product = Product.objects.create(
@@ -56,7 +58,6 @@ def add_product(request):
             reviews=reviews
         )
 
-        # Return success response with the product data
         return JsonResponse({
             'success': True,
             'product': {
@@ -66,4 +67,5 @@ def add_product(request):
                 'reviews': product.reviews,
             }
         })
-    return JsonResponse({'success': False, 'error': 'Invalid request method.'})
+    except Exception as e:
+        return JsonResponse({'success': False, 'error': str(e)})
