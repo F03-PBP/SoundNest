43e3829824f22d44e22fe19315595f74a31578f8
mikewallker <yosiajosephc@gmail.com>
2024-10-27 18:58:44 +0700

    fixing dislay

--- BestDeals/static/js/index.js
+++ BestDeals/static/js/index.js
@@ -228,17 +228,7 @@ document.addEventListener("DOMContentLoaded", function () {
                 </div>
                 
                 <div class="absolute top-2 right-2 flex flex-col gap-2">
-                    <button class="bg-white p-2 rounded-full shadow-md hover:bg-gray-100 transition-colors">
-                        <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
-                        </svg>
-                    </button>
-                    <button class="bg-white p-2 rounded-full shadow-md hover:bg-gray-100 transition-colors">
-                        <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
-                        </svg>
-                    </button>
+                    
                 </div>
     
                 <div class="aspect-square bg-gray-100">
